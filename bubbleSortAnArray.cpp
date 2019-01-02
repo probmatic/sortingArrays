237cea2c0146f4ab74a1dd3240169efc59285f23
Sarah Makky <probmatic@yahoo.com>
2019-01-02 13:21:30 -0500

    Create bubbleSortAnArray.cpp

---
+++ bubbleSortAnArray.cpp
@@ -0,0 +1,52 @@
+/*probmatic*/
+/*Using bubble sort to arrange numbers in an array in ascending order*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int n;
+  
+  cout << "Enter the amount of numbers you would like to sort:" << endl;
+  cin >> n;
+    
+  cout << "Enter " << n <<" integers to sort. Hit 'Enter' after each input: " << endl;
+	int numbArray[n] = {};
+  
+	for (int i = 0; i < n; i++)
+	{
+		cin >> numbArray[i];
+	}
+
+	cout << "The numbers you inputted are: ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << numbArray[i] << " ";
+	}
+
+	cout << endl << "Sorting into ascending order using a bubble sort:"<< endl;
+
+	for (int j = 0; j < n; j++)
+	{
+		for (int i = 0; i < n-1; i++)
+		{
+			if (numbArray[i] > numbArray[i + 1])
+			{
+				int temp = numbArray[i + 1];
+				numbArray[i + 1] = numbArray[i];
+				numbArray[i] = temp;
+			}
+		}
+	}
+	cout << "Numbers sorted are: ";
+	for (int i = 0; i < n ; i++)
+	{
+		cout << numbArray[i] << " ";
+	}
+
+	system("pause");
+	return 0;
+}
